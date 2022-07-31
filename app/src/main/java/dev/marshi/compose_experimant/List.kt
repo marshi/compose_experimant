fc9876d0e5968a1f9687f5610f73fe87f79742e8
marshi <mashi3648@gmail.como>
2022-07-31 19:22:13 +0900

    samples.

---
+++ app/src/main/java/dev/marshi/compose_experimant/List.kt
@@ -0,0 +1,149 @@
+package dev.marshi.compose_experimant
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.SnapSpec
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+@Composable
+fun List() {
+    LazyColumn() {
+        item {
+            AnimatedContentSample()
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AnimatedContentSample() {
+    var count by remember { mutableStateOf(0) }
+    Column() {
+        AnimatedContent(
+            targetState = count,
+            transitionSpec = {
+                // Compare the incoming number with the previous number.
+                if (targetState > initialState) {
+                    // If the target number is larger, it slides up and fades in
+                    // while the initial (smaller) number slides up and fades out.
+                    slideInVertically { height -> height } + fadeIn() with
+                            slideOutVertically { height -> -height } + fadeOut()
+                } else {
+                    // If the target number is smaller, it slides down and fades in
+                    // while the initial number slides down and fades out.
+                    slideInVertically { height -> -height } + fadeIn() with
+                            slideOutVertically { height -> height } + fadeOut()
+                }.using(
+                    // Disable clipping since the faded slide-in/out should
+                    // be displayed out of bounds.
+                    SizeTransform(clip = false)
+                )
+            }
+        ) { targetCount ->
+            Text(text = "$targetCount")
+        }
+        Button(onClick = { count++ }) {
+            Text("button")
+        }
+        Row() {
+            MarqueeText(text = "ariuaoeuoiauoijaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Slide() {
+    val width = 96.dp
+    val menuWidth = 30.dp
+
+    val swipeableState = rememberSwipeableState(0, confirmStateChange = {
+        println(it)
+        true
+    })
+    val density = LocalDensity.current
+    val menuWidthPx = with(density) { menuWidth.toPx() }
+    val anchors = mapOf(0f to 0, -menuWidthPx to 1) // Maps anchor points (in px) to states
+
+    Box(
+        modifier = Modifier
+            .width(width)
+            .swipeable(
+                state = swipeableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                orientation = Orientation.Horizontal,
+            )
+            .background(Color.LightGray)
+    ) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            val alpha = if (swipeableState.progress.to == 0) {
+                1 - swipeableState.progress.fraction
+            } else {
+                swipeableState.progress.fraction
+            }
+            Text(
+                modifier = Modifier
+                    .width(30.dp)
+                    .alpha(alpha)
+                    .align(Alignment.CenterEnd),
+                text = "aiueoaiuaiueo",
+            )
+            Box(
+                Modifier
+                    .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .background(Color.DarkGray)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SlidePreview() {
+    Slide()
+}
+
+@Preview
+@Composable
+fun AnimPreview() {
+    AnimatedContentSample()
+}
---
+++ app/src/main/java/dev/marshi/compose_experimant/MarqeeText.kt
@@ -0,0 +1,196 @@
+package dev.marshi.compose_experimant
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.TargetBasedAnimation
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+@Composable
+fun MarqueeText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textModifier: Modifier = Modifier,
+    gradientEdgeColor: Color = Color.White,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current,
+) {
+    val createText = @Composable { localModifier: Modifier ->
+        Text(
+            text,
+            textAlign = textAlign,
+            modifier = localModifier,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = 1,
+            onTextLayout = onTextLayout,
+            style = style,
+        )
+    }
+    var offset by remember { mutableStateOf(0) }
+    val textLayoutInfoState = remember { mutableStateOf<TextLayoutInfo?>(null) }
+    LaunchedEffect(textLayoutInfoState.value) {
+        val textLayoutInfo = textLayoutInfoState.value ?: return@LaunchedEffect
+        if (textLayoutInfo.textWidth <= textLayoutInfo.containerWidth) return@LaunchedEffect
+        // container幅分のスクロールに7500msかける
+        val duration = 7500 * textLayoutInfo.textWidth / textLayoutInfo.containerWidth
+        val delay = 1000L
+        val animation = TargetBasedAnimation(
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = duration,
+                    delayMillis = 1000,
+                    easing = LinearEasing,
+                ),
+                repeatMode = RepeatMode.Restart
+            ),
+            typeConverter = Int.VectorConverter,
+            initialValue = 0,
+            targetValue = -textLayoutInfo.textWidth
+        )
+        do {
+            val startTime = withFrameNanos { it }
+            do {
+                val playTime = withFrameNanos { it } - startTime
+                offset = (animation.getValueFromNanos(playTime))
+            } while (!animation.isFinishedFromNanos(playTime))
+            delay(delay)
+        } while (true)
+    }
+
+    SubcomposeLayout(
+        modifier = modifier.clipToBounds()
+    ) { constraints ->
+        val infiniteWidthConstraints = constraints.copy(maxWidth = Constraints.Infinity)
+        var mainText = subcompose(MarqueeLayers.MainText) {
+            createText(textModifier)
+        }.first().measure(infiniteWidthConstraints)
+
+        var gradient: Placeable? = null
+
+        var secondPlaceableWithOffset: Pair<Placeable, Int>? = null
+        if (mainText.width <= constraints.maxWidth) {
+            mainText = subcompose(MarqueeLayers.SecondaryText) {
+                createText(textModifier.fillMaxWidth())
+            }.first().measure(constraints)
+            textLayoutInfoState.value = null
+        } else {
+            val spacing = constraints.maxWidth * 2 / 3
+            textLayoutInfoState.value = TextLayoutInfo(
+                textWidth = mainText.width + spacing, // テキストの長さ + widthの2/3の長さ
+                containerWidth = constraints.maxWidth // 画面幅
+            )
+            val secondTextOffset =
+                mainText.width + spacing + offset // width + spacing + -(0 ~ width + spacing)
+            val secondTextSpace = constraints.maxWidth - secondTextOffset
+            if (secondTextSpace > 0) {
+                // 2週目以降のテキストのスタート位置が描画範囲内にある
+                secondPlaceableWithOffset = subcompose(MarqueeLayers.SecondaryText) {
+                    createText(textModifier)
+                }.first().measure(infiniteWidthConstraints) to secondTextOffset
+            }
+            gradient = subcompose(MarqueeLayers.EdgesGradient) {
+                Row {
+                    GradientEdge(gradientEdgeColor, Color.Transparent)
+                    Spacer(Modifier.weight(1f))
+                    GradientEdge(Color.Transparent, gradientEdgeColor)
+                }
+            }.first().measure(constraints.copy(maxHeight = mainText.height))
+        }
+
+        layout(
+            width = constraints.maxWidth,
+            height = mainText.height
+        ) {
+            mainText.place(offset, 0)
+            secondPlaceableWithOffset?.let { (placable, secondTextOffset) ->
+                placable.place(secondTextOffset, 0)
+            }
+            gradient?.place(0, 0)
+        }
+    }
+}
+
+@Composable
+private fun GradientEdge(
+    startColor: Color, endColor: Color,
+) {
+    Box(
+        modifier = Modifier
+            .width(10.dp)
+            .fillMaxHeight()
+            .background(
+                brush = Brush.horizontalGradient(
+                    0f to startColor, 1f to endColor,
+                )
+            )
+    )
+}
+
+@Preview
+@Composable
+fun MarqeeTextPreview() {
+    Column {
+        MarqueeText(text = "aa")
+        MarqueeText(text = "aiueoaiueo")
+    }
+}
+
+private enum class MarqueeLayers { MainText, SecondaryText, EdgesGradient }
+private data class TextLayoutInfo(val textWidth: Int, val containerWidth: Int)
